04aa2ed3d4bcb12edd177ecb7765955acf6e45d5
dvass01 <dvassiliou@hotmail.com>
2015-05-09 19:11:30 -0400

    developing views, base & questions html files

--- project/trivia/views.py
+++ project/trivia/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponse
+from django.db import models
+from users.models import User
+from users.forms import UserForm
+from trivia.models import Genre
 
-# Create your views here.
+# Create your models here.
+
+class GenreView(View):
+    template_name = 'trivia/index.html'
+    all_genres = Genre.objects.all()
+
+    def get(self, request):
+        active_user_id = request.session.get('user_id')
+        active_user = User.objects.filter(id=active_user_id)
+        return render(request, self.template_name, {'all_genres': self.all_genres, 'active_user': active_user})
+
+
+class QuestionView(View):
+    template_name = 'trivia/questions.html'
+    context_dict = {}
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ResultsView(View):
+    template_name = 'trivia/results.html'
+
+
+    def get(self, request):
+        pass
