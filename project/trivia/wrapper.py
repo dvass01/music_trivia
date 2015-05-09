c9b6178791625256ad144bf0ea773ed8576ad7a6
dvass01 <dvassiliou@hotmail.com>
2015-05-09 16:46:54 -0400

    wrapper file completed

--- project/trivia/wrapper.py
+++ project/trivia/wrapper.py
@@ -21,9 +21,11 @@ class EchoNest:
         search_result = requests.get(self.similar_artists_url + string)
         if search_result.json() != []:
             similar_artists_dict =search_result.json()
-            print(similar_artists_dict['response']['artists'][0]['name'])
+            # print(similar_artists_dict['response']['artists'][0]['name'])
             for artist in range(0, len(similar_artists_dict['response']['artists'])-1):
                 print(similar_artists_dict['response']['artists'][artist]['name'])
+                results_array.append(artist)
+            return results_array
 
         else:
             print("No artists found.")
@@ -32,20 +34,50 @@ class EchoNest:
     def get_genre_artists(self, string):
         results_array = []
         search_result = requests.get(self.genre_artists_url + string)
-        print(search_result.json())
+        if search_result.json() != []:
+            genre_artists_dict = search_result.json()
+            for artist in range(0, len(genre_artists_dict['response']['artists'])-1):
+                print(genre_artists_dict['response']['artists'][artist]['name'])
+                results_array.append(artist)
+            return results_array
+        else:
+            print("No artists found.")
+            return None
+
 
     def get_artist_songs(self, string):
         results_array = []
         search_result = requests.get(self.artist_songs_url + string)
-        print(search_result.json())
+        if search_result.json() != []:
+            artist_songs_dict = search_result.json()
+            for song in range(0, len(artist_songs_dict['response']['songs'])-1):
+                print(artist_songs_dict['response']['songs'][song]['title'])
+                results_array.append(song)
+            return results_array
+        else:
+            print("No songs found.")
+            return None
+
 
     def get_artist_biographies(self, string):
         results_array = []
         search_result = requests.get(self.biographies_url + string)
-        print(search_result.json())
+        if search_result.json() != []:
+            artist_biographies_dict = search_result.json()
+            print(artist_biographies_dict['response']['biographies'][0]['text'])
+            results_array.append(artist_biographies_dict['response']['biographies'][0]['text'])
+            return results_array
+        else:
+            print("No biography found.")
+            return None
+
 
 e = EchoNest()
-e.similar_artist_search('ledzeppelin')
-# e.get_genre_artists('rock')
-# e.get_artist_songs('ledzeppelin')
-# e.get_artist_biographies('blindfaith')
+e.similar_artist_search('rainbow')
+print()
+e.get_genre_artists('rock')
+print()
+e.get_artist_songs('deeppurple')
+print()
+e.get_artist_biographies('blindfaith')
+print()
