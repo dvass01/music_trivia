16f9dd728146100686ad7fa65adee6e392be8399
brendan gilroy <bdgilroy@gmail.com>
2015-05-09 20:47:05 -0400

    seeded genres in database; created Question model

--- project/trivia/models.py
+++ project/trivia/models.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.db import models
 
-# Create your views here.
+# Create your models here.
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=8)
+
+class Question(models.Model):
+    user_id = models.ForeignKey('users.User')
+    question_text = models.CharField(max_length=100)
+    choice_1 = models.CharField(max_length=100)
+    choice_2 = models.CharField(max_length=100)
+    choice_3 = models.CharField(max_length=100)
+    choice_4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    user_answer = models.IntegerField(null=True)
+
