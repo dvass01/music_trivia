53a12606d634d6e61efe85f471fe64fe5f0884ad
Connor Hammond <ConnorH2582@users.noreply.github.com>
2015-05-10 18:25:15 -0400

    Merges

--- users/fixtures.py
+++
@@ -1,9 +0,0 @@
-# import factory
-#
-# class UserFactory(factory.DjangoModelFactory):
-#     class Meta:
-#         model = 'users.User' # imports for you
-#
-#     username = 'new_user'
-#     first_name = 'Steve'
-#     last_name = 'Polykronopolous'
